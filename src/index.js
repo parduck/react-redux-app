99e38dfea5881aa831c4676b2522f0b47225f222
chankim <videoal@gmail.com>
2019-12-24 17:12:58 +0900

    redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,71 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState={
+  task:'',
+  tasks:[]
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function tasksReducer(state=initialState,action){
+  switch(action.type){
+    case 'INPUT_TASK':
+      return{
+        ...state,
+        task:action.payload.task
+      };
+    case 'ADD_TASK':
+      return{
+        ...state,
+        tasks:state.tasks.concat([action.payload.task])
+      };
+    default:
+      return state;
+  }
+}
+const store = createStore(tasksReducer);
+
+const inputTask = (task)=>({
+    type:'INPUT_TASK',
+    payload:{
+        task
+    }
+});
+
+const addTask=(task)=>({
+    type:'ADD_TASK',
+    payload:{
+        task
+    }
+});
+
+function TodoApp({store}){
+    const{task, tasks}=store.getState();
+    return(
+        <div>
+            <input type="text" onChange={(e)=>store.dispatch(inputTask(e.target.value))} />
+            <input type="button" value="add" onClick={()=>store.dispatch(addTask(task))} />
+            <ul>
+                {
+                    tasks.map(function(item,i){
+                        return(
+                            <li key={i}>{item}</li>
+                        )
+                    })
+                }
+            </ul>
+        </div>
+    );
+}
+
+function renderApp(store){
+    render(
+        <TodoApp store={store} />,
+        document.getElementById('root')
+    );
+}
+
+store.subscribe(()=>renderApp(store));
+renderApp(store);
+
+//export default App;
